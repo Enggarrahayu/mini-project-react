2a8d79351373aa51907055fba51b25b66e87c70c
Your Name <enggarrahayu64@gmail.com>
2025-03-14 22:43:58 +0700

    feat: add navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import logo from '../../assets/img/logo.png'
+import '../../assets/style/main.scss'
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <nav className="container flex items-center justify-between navbar">
+        {/* Logo */}
+        <div className="order-0">
+          <a>
+            <img src={logo} height="20" width="147" alt="logo" />
+          </a>
+        </div>
+
+        {/* Navbar Toggler */}
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="flex items-center order-1 cursor-pointer lg:hidden"
+        >
+          {isMenuOpen ? (
+            <svg className="h-6 fill-current" viewBox="0 0 20 20">
+              <title>Menu Close</title>
+              <polygon
+                points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+                transform="rotate(45 10 10)"
+              ></polygon>
+            </svg>
+          ) : (
+            <svg className="h-6 fill-current" viewBox="0 0 20 20">
+              <title>Menu Open</title>
+              <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z"></path>
+            </svg>
+          )}
+        </button>
+
+        {/* Navbar Menu */}
+        <ul
+          className={`navbar-nav order-2 w-full flex-[0_0_100%] lg:order-1 lg:flex lg:w-auto lg:flex-auto lg:justify-center lg:space-x-5 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          <li className="nav-item">
+            <a href="" className="nav-link active">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="about.html" className="nav-link">User List</a>
+          </li>         
+          <li className="nav-item">
+            <a href="contact.html" className="nav-link">About</a>
+          </li>
+          <li className="nav-item mt-3.5 lg:hidden">
+            <a className="btn btn-white btn-sm border-border" href="signin.html">Sign Up Now</a>
+          </li>
+        </ul>
+
+        <div className="items-center order-1 hidden ml-auto md:order-2 md:ml-0 lg:flex">
+          <a className="btn btn-white btn-sm" href="signin.html">Sign Up Now</a>
+        </div>
+      </nav>
+    </header>
+    
+  );
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,90 @@
+import theme from "./src/theme.json";
+
+/** Konversi font size */
+let font_base = Number(theme.fonts.font_size.base.replace("px", ""));
+let font_scale = Number(theme.fonts.font_size.scale);
+let h6 = font_base / font_base;
+let h5 = h6 * font_scale;
+let h4 = h5 * font_scale;
+let h3 = h4 * font_scale;
+let h2 = h3 * font_scale;
+let h1 = h2 * font_scale;
+
+/** Konversi font family */
+let fontPrimary = theme.fonts.font_family.primary
+  ? theme.fonts.font_family.primary
+      .replace(/\+/g, " ")
+      .replace(/:[ital,]*[ital@]*[wght@]*[0-9,;]+/gi, "")
+  : "sans-serif";
+
+let fontSecondary = theme.fonts.font_family.secondary
+  ? theme.fonts.font_family.secondary
+      .replace(/\+/g, " ")
+      .replace(/:[ital,]*[ital@]*[wght@]*[0-9,;]+/gi, "")
+  : "sans-serif";
+
+export default {
+  mode: "jit",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // React File Paths
+  ],
+  safelist: [{ pattern: /^swiper-/ }],
+  theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        text: theme.colors.default.text_color.default,
+        light: theme.colors.default.text_color.light,
+        dark: theme.colors.default.text_color.dark,
+        primary: theme.colors.default.theme_color.primary,
+        secondary: theme.colors.default.theme_color.secondary,
+        body: theme.colors.default.theme_color.body,
+        border: theme.colors.default.theme_color.border,
+        "theme-light": theme.colors.default.theme_color.theme_light,
+        "theme-dark": theme.colors.default.theme_color.theme_dark,
+      },
+      fontSize: {
+        base: font_base + "px",
+        h1: h1 + "rem",
+        "h1-sm": h1 * 0.8 + "rem",
+        h2: h2 + "rem",
+        "h2-sm": h2 * 0.8 + "rem",
+        h3: h3 + "rem",
+        "h3-sm": h3 * 0.8 + "rem",
+        h4: h4 + "rem",
+        h5: h5 + "rem",
+        h6: h6 + "rem",
+      },
+      fontFamily: {
+        primary: [fontPrimary, "sans-serif"],
+        secondary: [fontSecondary, "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("tailwind-bootstrap-grid")({
+      generateContainer: false,
+      gridGutterWidth: "2rem",
+      gridGutters: {
+        1: "0.25rem",
+        2: "0.5rem",
+        3: "1rem",
+        4: "1.5rem",
+        5: "3rem",
+      },
+    }),
+  ],
+};
